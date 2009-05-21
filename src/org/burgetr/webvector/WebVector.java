f42fe2b0e1a665177140de2c08adbe3f23c9c701
Radek Burget <burgetr@fit.vutbr.cz>
2009-05-21 15:07:45 +0000

    Visual face
    git-svn-id: svn+ssh://pcuifs2.fit.vutbr.cz/home/burgetr/svn/WebVector/trunk@3 d127e1e8-ba21-419e-a239-6e914434a7a0

--- src/org/burgetr/webvector/WebVector.java
+++ src/org/burgetr/webvector/WebVector.java
@@ -14,6 +14,12 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.Font;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
 
 /**
  * This is a wrapper class for calling the CSSBox ImageRenderer demo. 
@@ -26,6 +32,17 @@ public class WebVector
     private JFrame mainFrame = null;  //  @jve:decl-index=0:visual-constraint="415,61"
     private JPanel mainPanel = null;
     private JLabel urlLabel = null;
+    private JTextField urlText = null;
+    private JLabel destLabel = null;
+    private JTextField destText = null;
+    private JTextField statusText = null;
+    private JButton startButton = null;
+    private JButton browseUrlButton = null;
+    private JButton browseDestButton = null;
+    private JPanel typePanel = null;
+    private JRadioButton svgRadio = null;
+    private JLabel typeLabel = null;
+    private JRadioButton pngRadio = null;
     /**
      * This method initializes mainFrame	
      * 	
@@ -37,6 +54,7 @@ public class WebVector
         {
             mainFrame = new JFrame();
             mainFrame.setSize(new Dimension(509, 270));
+            mainFrame.setTitle("WebVector");
             mainFrame.setContentPane(getMainPanel());
         }
         return mainFrame;
@@ -51,21 +69,214 @@ public class WebVector
     {
         if (mainPanel == null)
         {
+            GridBagConstraints gridBagConstraints8 = new GridBagConstraints();
+            gridBagConstraints8.gridx = 0;
+            gridBagConstraints8.anchor = GridBagConstraints.WEST;
+            gridBagConstraints8.insets = new Insets(10, 0, 0, 0);
+            gridBagConstraints8.gridy = 4;
+            typeLabel = new JLabel();
+            typeLabel.setText("Output type");
+            GridBagConstraints gridBagConstraints7 = new GridBagConstraints();
+            gridBagConstraints7.gridx = 0;
+            gridBagConstraints7.gridwidth = 2;
+            gridBagConstraints7.fill = GridBagConstraints.BOTH;
+            gridBagConstraints7.gridy = 5;
+            GridBagConstraints gridBagConstraints6 = new GridBagConstraints();
+            gridBagConstraints6.gridx = 1;
+            gridBagConstraints6.gridy = 3;
+            GridBagConstraints gridBagConstraints5 = new GridBagConstraints();
+            gridBagConstraints5.gridx = 1;
+            gridBagConstraints5.gridy = 1;
+            GridBagConstraints gridBagConstraints4 = new GridBagConstraints();
+            gridBagConstraints4.gridx = 0;
+            gridBagConstraints4.gridwidth = 2;
+            gridBagConstraints4.gridy = 7;
+            GridBagConstraints gridBagConstraints3 = new GridBagConstraints();
+            gridBagConstraints3.fill = GridBagConstraints.BOTH;
+            gridBagConstraints3.gridy = 6;
+            gridBagConstraints3.weightx = 1.0;
+            gridBagConstraints3.insets = new Insets(10, 0, 10, 0);
+            gridBagConstraints3.gridwidth = 2;
+            gridBagConstraints3.gridx = 0;
+            GridBagConstraints gridBagConstraints2 = new GridBagConstraints();
+            gridBagConstraints2.fill = GridBagConstraints.BOTH;
+            gridBagConstraints2.gridy = 3;
+            gridBagConstraints2.weightx = 1.0;
+            gridBagConstraints2.gridx = 0;
+            GridBagConstraints gridBagConstraints0 = new GridBagConstraints();
+            gridBagConstraints0.anchor = GridBagConstraints.WEST;
+            urlLabel = new JLabel();
+            urlLabel.setText("Source URL");
+            GridBagConstraints gridBagConstraints1 = new GridBagConstraints();
+            gridBagConstraints1.gridx = 0;
+            gridBagConstraints1.anchor = GridBagConstraints.WEST;
+            gridBagConstraints1.insets = new Insets(10, 0, 0, 0);
+            gridBagConstraints1.gridy = 2;
+            destLabel = new JLabel();
+            destLabel.setText("Destination file");
+            GridBagConstraints gridBagConstraints = new GridBagConstraints();
+            gridBagConstraints.fill = GridBagConstraints.BOTH;
+            gridBagConstraints.gridy = 1;
+            gridBagConstraints.weightx = 1.0;
+            gridBagConstraints.gridx = 0;
             mainPanel = new JPanel();
             mainPanel.setLayout(new GridBagLayout());
-            mainPanel.add(getUrlLabel());
+            mainPanel.add(urlLabel, gridBagConstraints0);
+            mainPanel.add(getUrlText(), gridBagConstraints);
+            mainPanel.add(destLabel, gridBagConstraints1);
+            mainPanel.add(getDestText(), gridBagConstraints2);
+            mainPanel.add(getStatusText(), gridBagConstraints3);
+            mainPanel.add(getStartButton(), gridBagConstraints4);
+            mainPanel.add(getBrowseUrlButton(), gridBagConstraints5);
+            mainPanel.add(getBrowseDestButton(), gridBagConstraints6);
+            mainPanel.add(getTypePanel(), gridBagConstraints7);
+            mainPanel.add(typeLabel, gridBagConstraints8);
         }
         return mainPanel;
     }
     
-    private JLabel getUrlLabel()
+    /**
+     * This method initializes urlText	
+     * 	
+     * @return javax.swing.JTextField	
+     */
+    private JTextField getUrlText()
     {
-        if (urlLabel == null)
+        if (urlText == null)
         {
-            urlLabel = new JLabel();
-            urlLabel.setText("Source URL");
+            urlText = new JTextField();
+            urlText.setText("http://");
         }
-        return urlLabel;
+        return urlText;
+    }
+
+    /**
+     * This method initializes destText	
+     * 	
+     * @return javax.swing.JTextField	
+     */
+    private JTextField getDestText()
+    {
+        if (destText == null)
+        {
+            destText = new JTextField();
+            destText.setText("output.svg");
+        }
+        return destText;
+    }
+
+    /**
+     * This method initializes statusText	
+     * 	
+     * @return javax.swing.JTextField	
+     */
+    private JTextField getStatusText()
+    {
+        if (statusText == null)
+        {
+            statusText = new JTextField();
+            statusText.setText("Waiting for start");
+            statusText.setFont(new Font("Dialog", Font.PLAIN, 18));
+            statusText.setEditable(false);
+        }
+        return statusText;
+    }
+
+    /**
+     * This method initializes startButton	
+     * 	
+     * @return javax.swing.JButton	
+     */
+    private JButton getStartButton()
+    {
+        if (startButton == null)
+        {
+            startButton = new JButton();
+            startButton.setText("Start");
+        }
+        return startButton;
+    }
+
+    /**
+     * This method initializes browseUrlButton	
+     * 	
+     * @return javax.swing.JButton	
+     */
+    private JButton getBrowseUrlButton()
+    {
+        if (browseUrlButton == null)
+        {
+            browseUrlButton = new JButton();
+            browseUrlButton.setText("Browse...");
+            browseUrlButton.setToolTipText("Browse for local files");
+        }
+        return browseUrlButton;
+    }
+
+    /**
+     * This method initializes browseDestButton	
+     * 	
+     * @return javax.swing.JButton	
+     */
+    private JButton getBrowseDestButton()
+    {
+        if (browseDestButton == null)
+        {
+            browseDestButton = new JButton();
+            browseDestButton.setText("Browse...");
+        }
+        return browseDestButton;
+    }
+
+    /**
+     * This method initializes typePanel	
+     * 	
+     * @return javax.swing.JPanel	
+     */
+    private JPanel getTypePanel()
+    {
+        if (typePanel == null)
+        {
+            GridBagConstraints gridBagConstraints9 = new GridBagConstraints();
+            gridBagConstraints9.gridx = 1;
+            gridBagConstraints9.insets = new Insets(0, 10, 0, 0);
+            gridBagConstraints9.gridy = 0;
+            typePanel = new JPanel();
+            typePanel.setLayout(new GridBagLayout());
+            typePanel.add(getSvgRadio(), new GridBagConstraints());
+            typePanel.add(getPngRadio(), gridBagConstraints9);
+        }
+        return typePanel;
+    }
+
+    /**
+     * This method initializes svgRadio	
+     * 	
+     * @return javax.swing.JRadioButton	
+     */
+    private JRadioButton getSvgRadio()
+    {
+        if (svgRadio == null)
+        {
+            svgRadio = new JRadioButton();
+            svgRadio.setText("SVG Vector Graphics");
+        }
+        return svgRadio;
+    }
+
+    /**
+     * This method initializes pngRadio	
+     * 	
+     * @return javax.swing.JRadioButton	
+     */
+    private JRadioButton getPngRadio()
+    {
+        if (pngRadio == null)
+        {
+            pngRadio = new JRadioButton();
+            pngRadio.setText("PNG Bitmap Graphics");
+        }
+        return pngRadio;
     }
 
     public static void main(String[] args)
